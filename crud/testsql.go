68e4f597256b624358947a58b1346c56ee74a425
simesaba80 <simesaba80>
2024-05-17 16:48:03 +0900

    schemaの移動

--- crud/testsql.go
+++ crud/testsql.go
@@ -5,14 +5,13 @@ import (
 	"log"
 
 	"github.com/semikoron/korocupbackend/database"
-	"github.com/semikoron/korocupbackend/schema"
 )
 
 func Test() {
-	if _, err := database.DB.NewCreateTable().Model((*schema.User)(nil)).Exec(context.TODO()); err != nil {
+	if _, err := database.DB.NewCreateTable().Model((*database.User)(nil)).Exec(context.TODO()); err != nil {
 		log.Fatalln(err)
 	}
-	user := &schema.User{Name: "鈴木太郎"}
+	user := &database.User{Name: "鈴木太郎"}
 	if _, err := database.DB.NewInsert().Model(user).Exec(context.TODO()); err != nil {
 		log.Fatalln(err)
 	}
