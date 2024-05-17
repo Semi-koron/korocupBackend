c3ad5720a7d7015592b60eb735b77aabbda7be39
simesaba80 <simesaba80>
2024-05-17 21:44:34 +0900

    Userモデルの定義

--- database/model.go
+++ database/model.go
@@ -1,10 +1,21 @@
 package database
 
-import "github.com/uptrace/bun"
+import (
+	"time"
+
+	"github.com/uptrace/bun"
+)
 
 type User struct {
 	bun.BaseModel `bun:"table:users,alias:u"`
 
-	ID   int64 `bun:",pk,autoincrement"`
-	Name string
+	ID        int `bun:",pk,autoincrement"`
+	Name      string
+	Email     string `bun:",unique"`
+	Password  string
+	Icon      int
+	Profile   int
+	CreatedAt time.Time `bun:",nullzero,notnull,default:current_timestamp"`
+	UpdatedAt time.Time `bun:",nullzero,notnull,default:current_timestamp"`
+	DeletedAt time.Time `bun:",nullzero"`
 }
