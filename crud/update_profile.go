37c3ef639094db46472965ee3582e783f545edb1
Taku2515 <it.program.2357@gmail.com>
2024-05-19 02:14:52 +0900

    ユーザープロフィールの編集をする際の認証の作成

--- crud/update_profile.go
+++ crud/update_profile.go
@@ -18,14 +18,17 @@ func UpdateUserDb(
 	user database.User,
 	newName string,
 	newIcon int,
-	newProfile int) database.User {
+	newProfile int,
+	profileUpdateUid string) database.User {
 
-	database.DB.Model(&user).Updates(
-		database.User{
-			UserName: newName,
-			Icon:     newIcon,
-			Profile:  newProfile})
+	database.DB.Where("firebase_uid = ?", profileUpdateUid).First(&user)
 
+	database.DB.Debug().Model(&user).
+		Updates(
+			database.User{
+				UserName: newName,
+				Icon:     newIcon,
+				Profile:  newProfile})
 	return user
 
 }
