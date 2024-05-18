4bcbf31c36a7ec17e248c3eb173793f752ded5f3
simesaba80 <simesaba80>
2024-05-18 11:46:23 +0900

    テーブルとリレーションの変更

--- database/init.go
+++ database/init.go
@@ -11,13 +11,13 @@ func InitDB() {
 	}
 	if _, err := DB.NewCreateTable().
 		Model((*Post)(nil)).
-		ForeignKey(`("user_id") REFERENCES "users" ("id") ON DELETE CASCADE`).
+		ForeignKey(`("user_id") REFERENCES "users" ("user_id") ON DELETE CASCADE`).
 		Exec(context.TODO()); err != nil {
 		log.Fatalln(err)
 	}
 	if _, err := DB.NewCreateTable().Model((*Like)(nil)).
-		ForeignKey(`("user_id") REFERENCES "users" ("id") ON DELETE CASCADE`).
-		ForeignKey(`("post_id") REFERENCES "posts" ("id") ON DELETE CASCADE`).
+		ForeignKey(`("user_id") REFERENCES "users" ("user_id") ON DELETE CASCADE`).
+		ForeignKey(`("post_id") REFERENCES "posts" ("user_id") ON DELETE CASCADE`).
 		Exec(context.TODO()); err != nil {
 		log.Fatalln(err)
 	}
--- database/model.go
+++ database/model.go
@@ -9,24 +9,23 @@ import (
 type User struct {
 	bun.BaseModel `bun:"table:users,alias:u"`
 
-	ID          int    `bun:",pk,autoincrement"`
-	Name        string `bun:",notnull"`
-	Email       string `bun:",unique,notnull"`
-	Password    string `bun:",notnull"`
-	DisplayName string `bun:",notnull,unique"`
-	Icon        int
-	Profile     int
-	CreatedAt   time.Time `bun:",nullzero,notnull,default:current_timestamp"`
-	UpdatedAt   time.Time `bun:",nullzero,notnull,default:current_timestamp"`
-	DeletedAt   time.Time `bun:",nullzero"`
+	UserID    string `bun:",pk,notnull"`
+	Name      string `bun:",notnull"`
+	Email     string `bun:",unique,notnull"`
+	Password  string `bun:",notnull"`
+	Icon      int
+	Profile   int
+	CreatedAt time.Time `bun:",nullzero,notnull,default:current_timestamp"`
+	UpdatedAt time.Time `bun:",nullzero,notnull,default:current_timestamp"`
+	DeletedAt time.Time `bun:",nullzero"`
 }
 
 type Post struct {
 	bun.BaseModel `bun:"table:posts,alias:p"`
 
 	ID        int       `bun:",pk,autoincrement"`
-	UserID    int       `bun:",notnull"`
-	User      *User     `bun:"rel:belongs-to,join:user_id=id"`
+	UserID    string    `bun:",notnull"`
+	User      *User     `bun:"rel:belongs-to,join:user_name=user_name"`
 	Image     []byte    `bun:",notnull"`
 	Reply     int       `bun:",nullzero"`
 	Likes     int       `bun:",nullzero"`
@@ -38,8 +37,8 @@ type Post struct {
 type Like struct {
 	bun.BaseModel `bun:"table:likes,alias:l"`
 	ID            int       `bun:",pk,autoincrement"`
-	UserID        int       `bun:",notnull,unique:u_l"`
-	User          *User     `bun:"rel:belongs-to,join:user_id=id"`
+	UserName      string    `bun:",notnull,unique:u_l"`
+	User          *User     `bun:"rel:belongs-to,join:user_id=user_id"`
 	PostID        int       `bun:",notnull,unique:u_l"`
 	Post          *Post     `bun:"rel:belongs-to,join:post_id=id"`
 	CreatedAt     time.Time `bun:",nullzero,notnull,default:current_timestamp"`
