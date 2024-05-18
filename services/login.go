6d6e7bd73f3792331e9b622d1c5c9cff1d080f56
simesaba80 <simesaba80>
2024-05-19 04:06:40 +0900

    ログイン時レコードが見つからない以外のエラーハンドリングを追加

--- services/login.go
+++ services/login.go
@@ -15,6 +15,8 @@ func Login(c echo.Context) error {
 	user, err := crud.GetUser(uid)
 	if errors.Is(err, gorm.ErrRecordNotFound) {
 		return c.JSON(http.StatusNotFound, map[string]string{"error": "User not found"})
+	} else if err != nil {
+		return c.JSON(http.StatusInternalServerError, map[string]string{"error": "Internal server error"})
 	}
 	return c.JSON(http.StatusOK, user)
 }
